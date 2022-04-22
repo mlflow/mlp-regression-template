5c1633068da626389914e45b5c45e39d040495a5
Mark Zhang <markzhang.inbox@gmail.com>
2022-04-22 14:02:06 -0700

    Added skeleton pipeline template
    Signed-off-by: Mark Zhang <markzhang.inbox@gmail.com>

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mlflow-pipeline-sklearn-regression-example",
+    version="1.0",
+    description="An example use of MLflow pipeline based on a sklearn regression model.",
+    author="Databricks",
+    url="https://mlflow.org/",
+    packages=find_packages(),
+    install_requires=["mlflow", "scikit-learn", "pandas"],
+    license="Apache License 2.0",
+    python_requires=">=3.7",
+)
