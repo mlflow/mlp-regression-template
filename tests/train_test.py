011ea0abdf191ee5eddb21daf7c57d1caf6dd234
apurva-koti <apurva.koti@databricks.com>
2022-05-11 11:15:04 -0700

    remove
    Signed-off-by: apurva-koti <apurva.koti@databricks.com>

--- tests/train_test.py
+++ tests/train_test.py
@@ -11,30 +11,3 @@ def test_user_train_returns_object_with_correct_spec():
 def test_user_train_passes_check_estimator():
     regressor = train_fn()
     check_estimator(regressor)
-
-
-def test_user_train_on_fake_data():
-    from sklearn import datasets
-    iris = datasets.load_iris()
-    parameters = {"kernel": ("linear", "rbf"), "C": [1, 10]}
-    svc = svm.SVC()
-    clf = GridSearchCV(svc, parameters)
-
-    clf.fit(iris.data, iris.target)
-    run_id = mlflow.last_active_run().info.run_id
-
-    # show data logged in the parent run
-    print("========== parent run ==========")
-    for key, data in fetch_logged_data(run_id).items():
-        print("\n---------- logged {} ----------".format(key))
-        pprint(data)
-
-    # show data logged in the child runs
-    filter_child_runs = "tags.mlflow.parentRunId = '{}'".format(run_id)
-    runs = mlflow.search_runs(filter_string=filter_child_runs)
-    param_cols = ["params.{}".format(p) for p in parameters.keys()]
-    metric_cols = ["metrics.mean_test_score"]
-
-    print("\n========== child runs ==========\n")
-    pd.set_option("display.max_columns", None)  # prevent truncating columns
-    print(runs[["run_id", *param_cols, *metric_cols]])
