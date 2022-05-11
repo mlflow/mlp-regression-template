502c58ceff11a9c2fb2759f17fa59dba1f8d67a7
apurva-koti <apurva.koti@databricks.com>
2022-05-11 13:02:44 -0700

    rename
    Signed-off-by: apurva-koti <apurva.koti@databricks.com>

--- tests/train_test.py
+++ tests/train_test.py
@@ -2,12 +2,12 @@ from examples.pipelines.sklearn_regression.steps.train import train_fn
 from sklearn.utils.estimator_checks import check_estimator
 
 
-def test_user_train_returns_object_with_correct_spec():
+def test_train_fn_returns_object_with_correct_spec():
     regressor = train_fn()
     assert callable(getattr(regressor, "fit", None))
     assert callable(getattr(regressor, "predict", None))
 
 
-def test_user_train_passes_check_estimator():
+def test_train_fn_passes_check_estimator():
     regressor = train_fn()
     check_estimator(regressor)
