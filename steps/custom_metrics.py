7ffa7c2fbc96c4f5343ec89d42e5946859f19ec5
harupy <17039389+harupy@users.noreply.github.com>
2022-05-18 14:55:34 +0900

    use more realistic custom metric
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- steps/custom_metrics.py
+++ steps/custom_metrics.py
@@ -1,2 +1,9 @@
-def always_one_fn(eval_df, builtin_metrics):
-    return {"always_one": 1}
+from sklearn.metrics import mean_squared_error
+
+
+def weighted_mean_squared_error(eval_df, builtin_metrics):
+    return {
+        "weighted_mean_squared_error": mean_squared_error(
+            eval_df["prediction"], eval_df["target"], sample_weight=1 / eval_df["prediction"].values
+        )
+    }
