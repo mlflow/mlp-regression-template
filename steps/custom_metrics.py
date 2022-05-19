759cebbc0f4aa7b56fecd0b7368dc6bb65584e02
harupy <17039389+harupy@users.noreply.github.com>
2022-05-19 11:15:22 +0900

    improve tests
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- steps/custom_metrics.py
+++ steps/custom_metrics.py
@@ -4,6 +4,8 @@ from sklearn.metrics import mean_squared_error
 def weighted_mean_squared_error(eval_df, builtin_metrics):
     return {
         "weighted_mean_squared_error": mean_squared_error(
-            eval_df["prediction"], eval_df["target"], sample_weight=1 / eval_df["prediction"].values
+            eval_df["prediction"],
+            eval_df["target"],
+            sample_weight=1 / eval_df["prediction"].values,
         )
     }
