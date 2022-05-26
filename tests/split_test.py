d9adbdbe423b935c2a4d216c050e86e274fe7c9e
apurva-koti <apurva.koti@databricks.com>
2022-05-26 13:42:37 -0700

    fix
    Signed-off-by: apurva-koti <apurva.koti@databricks.com>

--- tests/split_test.py
+++ tests/split_test.py
@@ -1,4 +1,4 @@
-from examples.pipelines.sklearn_regression.steps.split import split_fn
+from examples.pipelines.sklearn_regression.steps.split import process_splits
 from pandas import DataFrame
 
 
@@ -6,7 +6,7 @@ def test_split_fn_returns_datasets_with_correct_spec():
     train = DataFrame({"a": [1, 2, 3], "b": [4, 5, 6], "c": [7, 8, 9]})
     validation = DataFrame({"a": [1], "b": [4], "c": [7]})
     test = DataFrame({"a": [2], "b": [6], "c": [9]})
-    (train_processed, validation_processed, test_processed) = split_fn(train, validation, test)
+    (train_processed, validation_processed, test_processed) = process_splits(train, validation, test)
     assert isinstance(train_processed, DataFrame)
     assert isinstance(validation_processed, DataFrame)
     assert isinstance(test_processed, DataFrame)
