d3cf242f0125b7d6e313a36d2b4ad6c336c17e96
apurva-koti <apurva.koti@databricks.com>
2022-05-27 11:27:39 -0700

    temp
    Signed-off-by: apurva-koti <apurva.koti@databricks.com>

--- steps/split.py
+++ steps/split.py
@@ -7,7 +7,7 @@ def process_splits(
     """
     Perform additional processing on the split datasets.
     """
-    def process(ds):
+    def process(ds: DataFrame):
         # Drop invalid data points
         cleaned = ds.dropna()
         # Filter out invalid fare amounts, trip distance, tip amounts, passenger counts
@@ -17,6 +17,13 @@ def process_splits(
         # Locations 264 and 265 map to unknown. Filter them out
         cleaned = cleaned[(cleaned["PULocationID"] < 264) & (cleaned["DOLocationID"] < 264)]
 
-        return cleaned
+        cleaned["pickup_dow"] = cleaned["tpep_pickup_datetime"].dt.dayofweek
+        cleaned["pickup_hour"] = cleaned["tpep_pickup_datetime"].dt.hour
+        trip_duration = cleaned["tpep_dropoff_datetime"] - cleaned["tpep_pickup_datetime"]
+        cleaned["trip_duration"] = trip_duration.map(lambda x: x.total_seconds() / 60)
+
+        # Large dataset. Take first 10%
+        length = ds.size
+        return cleaned.head(length/10)
     return process(train_df), process(validation_df), process(test_df)
 
--- steps/transform.py
+++ steps/transform.py
@@ -1,19 +1,15 @@
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler, PolynomialFeatures
 from sklearn.impute import SimpleImputer
 from sklearn.compose import make_column_transformer, make_column_selector
 
 
 def transform_fn():
-    transformer = make_column_transformer(
-        (
-            OneHotEncoder(handle_unknown="ignore"),
-            make_column_selector(dtype_include="object"),
-        ),
-        (
-            SimpleImputer(strategy="most_frequent"),
-            make_column_selector(dtype_include="number"),
-        ),
-        remainder="drop",
-    )
+    #         train_X = cleaned[["pickup_dow", "pickup_hour", "trip_distance", "trip_duration"]]
+    ct_pipe = ColumnTransformer(transformers=[
+        ('hour_encoder', OneHotEncoder(categories='auto', sparse=False), ["pickup_hour"]),
+        ('day_encoder', OneHotEncoder(categories='auto', sparse=False), ["pickup_dow"]),
+        ('std_scaler', StandardScaler(), ["trip_distance", "trip_duration"])
+    ])
 
     return transformer
