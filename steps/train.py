515d807f41db7a343306afd682de05a60cf54373
apurva-koti <apurva.koti@databricks.com>
2022-05-27 14:15:16 -0700

    temp
    Signed-off-by: apurva-koti <apurva.koti@databricks.com>

--- steps/train.py
+++ steps/train.py
@@ -1,7 +1,4 @@
 def train_fn():
-    # from sklearn.linear_model import ElasticNet
-    # return ElasticNet()
-
     from sklearn.linear_model import SGDRegressor
 
     return SGDRegressor()
--- steps/transform.py
+++ steps/transform.py
@@ -1,15 +1,24 @@
+from pandas import DataFrame
 from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder, StandardScaler, PolynomialFeatures
-from sklearn.impute import SimpleImputer
-from sklearn.compose import make_column_transformer, make_column_selector
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler, FunctionTransformer
+
+
+def compute_features(df: DataFrame):
+    df.loc[:, "pickup_dow"] = df["tpep_pickup_datetime"].dt.dayofweek
+    df.loc[:, "pickup_hour"] = df["tpep_pickup_datetime"].dt.hour
+    trip_duration = df["tpep_dropoff_datetime"] - df["tpep_pickup_datetime"]
+    df.loc[:, "trip_duration_min"] = trip_duration.map(lambda x: x.total_seconds() / 60.0)
+    return df
 
 
 def transform_fn():
-    #         train_X = cleaned[["pickup_dow", "pickup_hour", "trip_distance", "trip_duration"]]
-    ct_pipe = ColumnTransformer(transformers=[
-        ('hour_encoder', OneHotEncoder(categories='auto', sparse=False), ["pickup_hour"]),
-        ('day_encoder', OneHotEncoder(categories='auto', sparse=False), ["pickup_dow"]),
-        ('std_scaler', StandardScaler(), ["trip_distance", "trip_duration"])
-    ])
-
-    return transformer
+    return Pipeline(
+        steps=[
+            ('extra_features', FunctionTransformer(compute_features, validate=False)),
+            ('encoder', ColumnTransformer(transformers=[
+                ('hour_encoder', OneHotEncoder(categories='auto', sparse=False), ["pickup_hour"]),
+                ('day_encoder', OneHotEncoder(categories='auto', sparse=False), ["pickup_dow"]),
+                ('std_scaler', StandardScaler(), ["trip_distance", "trip_duration_min"])])
+             )
+        ])
