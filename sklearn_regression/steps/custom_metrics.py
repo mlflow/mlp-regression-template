8d2bee515bdc37f2c26550950e3cf95601aec2a4
Sunish Sheth <sunishsheth2009@gmail.com>
2022-06-03 13:59:59 -0700

    Fixing lint issues
    Signed-off-by: Sunish Sheth <sunishsheth2009@gmail.com>

--- sklearn_regression/steps/custom_metrics.py
+++ sklearn_regression/steps/custom_metrics.py
@@ -1,7 +1,9 @@
 from sklearn.metrics import mean_squared_error
 
 
-def weighted_mean_squared_error(eval_df, builtin_metrics):
+def weighted_mean_squared_error(
+    eval_df, builtin_metrics
+):  # pylint: disable=unused-argument
     return {
         "weighted_mean_squared_error": mean_squared_error(
             eval_df["prediction"],
