0b8f3cd799245677a16096fd50674202fd79702f
apurva-koti <apurva.koti@databricks.com>
2022-06-08 12:24:10 -0700

    tests
    Signed-off-by: apurva-koti <apurva.koti@databricks.com>

--- tests/split_test.py
+++ tests/split_test.py
@@ -1,11 +1,19 @@
-from steps.split import process_splits
+import pytest
+import os
+import pandas as pd
 from pandas import DataFrame
+from steps.split import process_splits
 
 
-def test_post_split_fn_returns_datasets_with_correct_spec():
-    train = DataFrame({"a": [1, 2, 3], "b": [4, 5, 6], "c": [7, 8, 9]})
-    validation = DataFrame({"a": [1], "b": [4], "c": [7]})
-    test = DataFrame({"a": [2], "b": [6], "c": [9]})
+@pytest.fixture
+def sample_data():
+    return pd.read_parquet(os.path.join(os.path.dirname(__file__), "test_sample.parquet"))
+
+
+def test_post_split_fn_returns_datasets_with_correct_spec(sample_data):
+    train = sample_data[0:3]
+    validation = sample_data[4:7]
+    test = sample_data[7:10]
     (train_processed, validation_processed, test_processed) = process_splits(
         train, validation, test
     )
@@ -14,10 +22,10 @@ def test_post_split_fn_returns_datasets_with_correct_spec():
     assert isinstance(test_processed, DataFrame)
 
 
-def test_post_split_fn_returns_non_empty_datasets():
-    train = DataFrame({"a": [1, 2, 3], "b": [4, 5, 6], "c": [7, 8, 9]})
-    validation = DataFrame({"a": [1], "b": [4], "c": [7]})
-    test = DataFrame({"a": [2], "b": [6], "c": [9]})
+def test_post_split_fn_returns_non_empty_datasets(sample_data):
+    train = sample_data[0:3]
+    validation = sample_data[4:7]
+    test = sample_data[7:10]
     (train_processed, validation_processed, test_processed) = process_splits(
         train, validation, test
     )
