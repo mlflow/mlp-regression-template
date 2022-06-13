107be5da70aaa7c87e593c8e7d292b55f81b1604
jinzhang21 <jin.zhang@databricks.com>
2022-06-13 16:51:58 -0700

    Fixed unit tests
    Signed-off-by: jinzhang21 <jin.zhang@databricks.com>

--- tests/transform_test.py
+++ tests/transform_test.py
@@ -1,7 +1,7 @@
-from steps.transform import transform_fn
+from steps.transform import transformer_fn
 
 
 def test_tranform_fn_returns_object_with_correct_spec():
-    transformer = transform_fn()
+    transformer = transformer_fn()
     assert callable(getattr(transformer, "fit", None))
     assert callable(getattr(transformer, "transform", None))
