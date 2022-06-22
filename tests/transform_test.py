14052f84f1903b7e41d0b7e763e7a01eb1edd0a6
Kasey Uhlenhuth <1402694+kuhlenh@users.noreply.github.com>
2022-06-22 15:51:00 -0500

    Update transform_test.py
    added test

--- tests/transform_test.py
+++ tests/transform_test.py
@@ -5,3 +5,4 @@ def test_tranform_fn_returns_object_with_correct_spec():
     transformer = transformer_fn()
     assert callable(getattr(transformer, "fit", None))
     assert callable(getattr(transformer, "transform", None))
+    assert callable(getattr(transformer, "transform", None))
