686dba5d43ad0ba5af16d9d0e8d2511488756b2c
Jin Zhang <jin.zhang@databricks.com>
2022-06-22 10:41:13 -0700

    Removed extra features in transformer
    Signed-off-by: Jin Zhang <jin.zhang@databricks.com>

--- steps/transform.py
+++ steps/transform.py
@@ -4,23 +4,12 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder, StandardScaler, FunctionTransformer
 
 
-def compute_features(df: DataFrame):
-    df.loc[:, "pickup_dow"] = df["tpep_pickup_datetime"].dt.dayofweek
-    df.loc[:, "pickup_hour"] = df["tpep_pickup_datetime"].dt.hour
-    trip_duration = df["tpep_dropoff_datetime"] - df["tpep_pickup_datetime"]
-    df.loc[:, "trip_duration_min"] = trip_duration.map(
-        lambda x: x.total_seconds() / 60.0
-    )
-    return df
-
-
 def transformer_fn():
     """Returns an UNFITTED transformer with fit() and transform() methods.
     Their input and output signatures should be compatible with sklearn transformers.
     """
     return Pipeline(
         steps=[
-            ("extra_features", FunctionTransformer(compute_features, validate=False)),
             (
                 "encoder",
                 ColumnTransformer(
@@ -38,7 +27,7 @@ def transformer_fn():
                         (
                             "std_scaler",
                             StandardScaler(),
-                            ["trip_distance", "trip_duration_min"],
+                            ["trip_distance", "trip_duration"],
                         ),
                     ]
                 ),
