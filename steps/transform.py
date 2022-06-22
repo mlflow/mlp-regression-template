1f6e1b28acac23cc47621138ab2b1e4aed1654a1
jinzhang21 <78067366+jinzhang21@users.noreply.github.com>
2022-06-22 10:54:09 -0700

    Removed extra features in transformer (#29)
    * Removed extra features in transformer
    
    Signed-off-by: Jin Zhang <jin.zhang@databricks.com>
    
    * Fixed lint
    
    Signed-off-by: Jin Zhang <jin.zhang@databricks.com>

--- steps/transform.py
+++ steps/transform.py
@@ -1,17 +1,6 @@
-from pandas import DataFrame
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder, StandardScaler, FunctionTransformer
-
-
-def compute_features(df: DataFrame):
-    df.loc[:, "pickup_dow"] = df["tpep_pickup_datetime"].dt.dayofweek
-    df.loc[:, "pickup_hour"] = df["tpep_pickup_datetime"].dt.hour
-    trip_duration = df["tpep_dropoff_datetime"] - df["tpep_pickup_datetime"]
-    df.loc[:, "trip_duration_min"] = trip_duration.map(
-        lambda x: x.total_seconds() / 60.0
-    )
-    return df
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
 
 def transformer_fn():
@@ -20,7 +9,6 @@ def transformer_fn():
     """
     return Pipeline(
         steps=[
-            ("extra_features", FunctionTransformer(compute_features, validate=False)),
             (
                 "encoder",
                 ColumnTransformer(
@@ -38,7 +26,7 @@ def transformer_fn():
                         (
                             "std_scaler",
                             StandardScaler(),
-                            ["trip_distance", "trip_duration_min"],
+                            ["trip_distance", "trip_duration"],
                         ),
                     ]
                 ),
