e506d327879ca36ece6cb2cae02fbb7304e1791b
Corey Zumar <39497902+dbczumar@users.noreply.github.com>
2022-06-23 14:40:48 -0700

    API doc improvements to user code in steps/*.py (#32)
    * Format & improve py fns
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * Phrasing
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * Format
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * Revert unnecessary reformatting
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * Fix split
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * DF consistency
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * Fix
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * Address comments
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * Lint
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * Format
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>

--- steps/train.py
+++ steps/train.py
@@ -1,6 +1,16 @@
+"""
+This module defines the following routines used by the 'train' step of the regression pipeline:
+
+- ``estimator_fn``: Defines the customizable estimator type and parameters that are used
+  during training to produce a model pipeline.
+"""
+
+
 def estimator_fn():
-    """Returns an UNFITTED estimator that contains fit() and predict() method.
-    Their input and output signatures should be compatible with sklearn estimators.
+    """
+    Returns an *unfitted* estimator that defines ``fit()`` and ``predict()`` methods.
+    The estimator's input and output signatures should be compatible with scikit-learn
+    estimators.
     """
     from sklearn.linear_model import SGDRegressor
 
