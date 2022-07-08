16f9a2c2c386411f6b4536334396b961deddae17
Bilal <ahmed.bilal@databricks.com>
2022-07-08 06:37:41 -0700

    test

--- .github/integration_test/notebook.py
+++ .github/integration_test/notebook.py
@@ -1,4 +1,24 @@
 # Databricks notebook source
 
+# MAGIC %md
+# MAGIC # MLflow Regression Pipeline Databricks Notebook
+# MAGIC This notebook runs the MLflow Regression Pipeline on Databricks and inspects its results.
+# MAGIC
+# MAGIC For more information about the MLflow Regression Pipeline, including usage examples,
+# MAGIC see the [Regression Pipeline overview documentation](https://mlflow.org/docs/latest/pipelines.html#regression-pipeline)
+# MAGIC and the [Regression Pipeline API documentation](https://mlflow.org/docs/latest/python_api/mlflow.pipelines.html#module-mlflow.pipelines.regression.v1.pipeline).
+
 # COMMAND ----------
-print(2)
+
+# MAGIC %pip install mlflow[pipelines]
+# MAGIC %pip install -r ../requirements.txt
+
+# COMMAND ----------
+
+from mlflow.pipelines import Pipeline
+
+p = Pipeline(profile="databricks")
+
+# COMMAND ----------
+
+p.run("evaluate")
