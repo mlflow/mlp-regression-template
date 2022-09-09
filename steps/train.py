a351cb520a582a4d7b70ab5c922d3bca09d41206
Prithvi Kannan <46332835+prithvikannan@users.noreply.github.com>
2022-09-09 10:46:58 -0700

    adding estimator args to estimator fn in train step (#75)
    * adding kwargs to train step
    
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>
    
    * use dict instead of kwarg
    
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>
    
    * default estimator params to empty dict
    
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>
    
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>

--- steps/train.py
+++ steps/train.py
@@ -6,7 +6,7 @@ This module defines the following routines used by the 'train' step of the regre
 """
 
 
-def estimator_fn():
+def estimator_fn(estimator_params={}):
     """
     Returns an *unfitted* estimator that defines ``fit()`` and ``predict()`` methods.
     The estimator's input and output signatures should be compatible with scikit-learn
@@ -14,4 +14,4 @@ def estimator_fn():
     """
     from sklearn.linear_model import SGDRegressor
 
-    return SGDRegressor(random_state=42)
+    return SGDRegressor(random_state=42, **estimator_params)
