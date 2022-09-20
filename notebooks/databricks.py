5fb69eef81162c259fd98e3f621e2358e80fc481
prithvi.kannan@databricks.com <prithvi.kannan@databricks.com>
2022-09-20 23:17:07 +0000

    install from feature branch

--- notebooks/databricks.py
+++ notebooks/databricks.py
@@ -1,16 +1,15 @@
 # Databricks notebook source
-
 # MAGIC %md
 # MAGIC # MLflow Regression Pipeline Databricks Notebook
 # MAGIC This notebook runs the MLflow Regression Pipeline on Databricks and inspects its results.
-# MAGIC
+# MAGIC 
 # MAGIC For more information about the MLflow Regression Pipeline, including usage examples,
 # MAGIC see the [Regression Pipeline overview documentation](https://mlflow.org/docs/latest/pipelines.html#regression-pipeline)
 # MAGIC and the [Regression Pipeline API documentation](https://mlflow.org/docs/latest/python_api/mlflow.pipelines.html#module-mlflow.pipelines.regression.v1.pipeline).
 
 # COMMAND ----------
 
-# MAGIC %pip install mlflow[pipelines]
+# MAGIC %pip install git+https://github.com/mlflow/mlflow@mlp-hyperparameter-tuning
 # MAGIC %pip install -r ../requirements.txt
 
 # COMMAND ----------
