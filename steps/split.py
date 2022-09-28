54835c95cc48c906250d55869ef8cecf00091934
Prithvi Kannan <prithvi.kannan@databricks.com>
2022-09-28 16:03:52 -0700

    move processing to mlflow side
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>

--- steps/split.py
+++ steps/split.py
@@ -1,32 +1,12 @@
 """
 This module defines the following routines used by the 'split' step of the regression pipeline:
 
-- ``process_splits``: Defines customizable logic for processing & cleaning the training, validation,
+- ``mark_to_be_filtered``: Defines customizable logic for filtering the training, validation,
   and test datasets produced by the data splitting procedure. Note that arbitrary transformations
   should go into the transform step.
 """
 
 from pandas import DataFrame, Series
-
-
-def process_splits(
-    train_df: DataFrame, validation_df: DataFrame, test_df: DataFrame
-) -> (DataFrame, DataFrame, DataFrame):
-    """
-    Perform additional processing on the split datasets.
-
-    :param train_df: The training dataset produced by the data splitting procedure.
-    :param validation_df: The validation dataset produced by the data splitting procedure.
-    :param test_df: The test dataset produced by the data splitting procedure.
-    :return: A tuple containing, in order: the processed training dataset, the processed
-             validation dataset, and the processed test dataset.
-    """
-
-    return (
-        train_df[mark_to_be_filtered(train_df)],
-        validation_df[mark_to_be_filtered(validation_df)],
-        test_df[mark_to_be_filtered(test_df)],
-    )
 
 
 def mark_to_be_filtered(dataset: DataFrame) -> Series(bool):
