2bdd3c68d1549be488af3db7e4bedf6083855056
Prithvi Kannan <prithvi.kannan@databricks.com>
2022-09-28 15:57:27 -0700

    figured it out
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>

--- steps/split.py
+++ steps/split.py
@@ -6,7 +6,6 @@ This module defines the following routines used by the 'split' step of the regre
   should go into the transform step.
 """
 
-from typing import Tuple
 from pandas import DataFrame, Series
 
 
@@ -23,27 +22,10 @@ def process_splits(
              validation dataset, and the processed test dataset.
     """
 
-    def process(df: DataFrame):
-        # Drop invalid data points
-        cleaned = df.dropna()
-        # Filter out invalid fare amounts and trip distance
-        cleaned = cleaned[
-            (cleaned["fare_amount"] > 0)
-            & (cleaned["trip_distance"] < 400)
-            & (cleaned["trip_distance"] > 0)
-            & (cleaned["fare_amount"] < 1000)
-        ]
-
-        return cleaned
-
-    # return process(train_df), process(validation_df), process(test_df)
-    train_df['to_filter'] = mark_to_be_filtered(train_df)
-    validation_df['to_filter'] = mark_to_be_filtered(validation_df)
-    test_df['to_filter'] = mark_to_be_filtered(test_df)
     return (
-        train_df[train_df.to_filter == True],
-        validation_df[validation_df.to_filter == True],
-        test_df[test_df.to_filter == True]
+        train_df[mark_to_be_filtered(train_df)],
+        validation_df[mark_to_be_filtered(validation_df)],
+        test_df[mark_to_be_filtered(test_df)],
     )
 
 
@@ -55,6 +37,9 @@ def mark_to_be_filtered(dataset: DataFrame) -> Series(bool):
     :param dataset: The {train,validation,test} dataset produced by the data splitting procedure.
     :return: A Series indicating whether each row should be filtered
     """
-    return (dataset["fare_amount"] > 0) & (dataset["trip_distance"] < 400) & (
-        dataset["trip_distance"] > 0
-    ) & (dataset["fare_amount"] < 1000) or dataset.isna().any(axis=1)
+    return (
+        (dataset["fare_amount"] > 0)
+        & (dataset["trip_distance"] < 400)
+        & (dataset["trip_distance"] > 0)
+        & (dataset["fare_amount"] < 1000)
+    ) | (~dataset.isna().any(axis=1))
