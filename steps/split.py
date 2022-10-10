40ac9f99261f59709a0a7e7c49a228b04f09d659
Prithvi Kannan <46332835+prithvikannan@users.noreply.github.com>
2022-10-10 16:45:29 -0700

    boolean post split fn  (#84)
    * update post_split_method yaml
    
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>
    
    * update split.py docs
    
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>
    
    * still doesnt work
    
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>
    
    * figured it out
    
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>
    
    * move processing to mlflow side
    
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>
    
    * rename to post_split_filter_method
    
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>
    
    * rename to create_dataset_filter
    
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>
    
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>

--- steps/split.py
+++ steps/split.py
@@ -1,26 +1,22 @@
 """
 This module defines the following routines used by the 'split' step of the regression pipeline:
 
-- ``process_splits``: Defines customizable logic for processing & cleaning the training, validation,
+- ``create_dataset_filter``: Defines customizable logic for filtering the training, validation,
   and test datasets produced by the data splitting procedure. Note that arbitrary transformations
   should go into the transform step.
 """
 
-from pandas import DataFrame
+from pandas import DataFrame, Series
 
 
-def process_splits(
-    train_df: DataFrame, validation_df: DataFrame, test_df: DataFrame
-) -> (DataFrame, DataFrame, DataFrame):
+def create_dataset_filter(dataset: DataFrame) -> Series(bool):
     """
-    Perform additional processing on the split datasets.
+    Mark rows of the split datasets to be additionally filtered. This function will be called on
+    the training, validation, and test datasets.
 
-    :param train_df: The training dataset produced by the data splitting procedure.
-    :param validation_df: The validation dataset produced by the data splitting procedure.
-    :param test_df: The test dataset produced by the data splitting procedure.
-    :return: A tuple containing, in order: the processed training dataset, the processed
-             validation dataset, and the processed test dataset.
+    :param dataset: The {train,validation,test} dataset produced by the data splitting procedure.
+    :return: A Series indicating whether each row should be filtered
     """
     # FIXME::OPTIONAL: implement post-split processing on the dataframes, such as data cleaning.
 
-    return train_df, validation_df, test_df
+    return Series(False, index=dataset.index)
